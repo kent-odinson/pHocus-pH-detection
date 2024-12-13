abb182a67aa5892a4a646ec4aecd2dfbc1134643
Kent Frederick Wirawan <kent.f.wirawan@gmail.com>
2024-12-13 15:53:14 +0700

    Add files via upload

--- page4.py
+++ page4.py
@@ -12,7 +12,10 @@ from io import BytesIO
 
 url1 = "https://drive.google.com/uc?export=download&id=1Tj0anph6-lVTRn16FVwWivIYU027Tosy"
 response1 = requests.get(url1)
+url5 = "https://drive.google.com/uc?export=download&id=1ySTOr2vCN15akwJpB_jVJCooNEZcAvv2"
+response5 = requests.get(url5)
 logo = Image.open(BytesIO(response1.content))
+kent = Image.open(BytesIO(response5.content))
 
 # Background and Theme Adjustment
 st.markdown(
@@ -29,20 +32,23 @@ st.markdown(
 )
 
 # App title
-tit1, tit2 = st.columns([3, 4])
+tit1, tit2 = st.columns([1,3])
 with tit1:
-    st.image(logo)
+    st.image(logo, width=300)
 with tit2:
-    st.header("pHocus Smart Patch")
-    st.write("### Heal. Track. Predict.")
+    st.write("## pHocus Smart Patch")
 st.write("""
 ##### A pH detector to measure your skin pH from just a photo of patch!
 """)
 
 # About section
-st.write(""" #### Meet The Team: Kelompok 1 Desain Proyek Teknik Biomedik 2021
-Debby Rofiko Malik (2106705000)
-Syafamillah Tsabitah (2106631816)  
-Kent Frederick Wirawan(2106706760)
-Fatimah Azzahra (2106732235)
-""")
+team1, team2 = st.columns([1,3])
+with team1:
+    st.image(kent, width=200)
+with team2:
+    st.write("Kent Frederick Wirawan(2106706760)")
+# st.write(""" #### Meet The Team: Kelompok 1 Desain Proyek Teknik Biomedik 2021
+# Debby Rofiko Malik (2106705000)
+# Syafamillah Tsabitah (2106631816)  
+# Fatimah Azzahra (2106732235)
+# """)

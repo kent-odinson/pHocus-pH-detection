8de8f1f385e960fa55150540e240120fda5b7cf8
Kent Frederick Wirawan <kent.f.wirawan@gmail.com>
2024-12-13 15:09:31 +0700

    Add files via upload

--- page1.py
+++ page1.py
@@ -32,9 +32,9 @@ st.markdown(
 )
 
 # App title
-tit1, tit2 = st.columns([1, 2])
+tit1, tit2 = st.columns([2])
 with tit1:
-    st.image(logo)
+    st.image(logo, width='50%')
 with tit2:
     st.header("pHocus Smart Patch")
     st.write("### Heal. Track. Predict.")

ed3f2edf5fb0d46d4f2c52eb752f20de1717efa1
Kent Frederick Wirawan <kent.f.wirawan@gmail.com>
2024-12-13 16:04:19 +0700

    Add files via upload

--- page4.py
+++ page4.py
@@ -12,10 +12,19 @@ from io import BytesIO
 
 url1 = "https://drive.google.com/uc?export=download&id=1Tj0anph6-lVTRn16FVwWivIYU027Tosy"
 response1 = requests.get(url1)
+url5 = "https://drive.google.com/uc?export=download&id=1Vaiutx8UktOjT_k1QpLD7V7RjWqN3yAT"
+response5 = requests.get(url5)
+url6 = "https://drive.google.com/uc?export=download&id=1ijbwFP43NehEwuosPz11dmX46B5rboXn"
+response6 = requests.get(url6)
 url7 = "https://drive.google.com/uc?export=download&id=1ySTOr2vCN15akwJpB_jVJCooNEZcAvv2"
 response7 = requests.get(url7)
+url8 = "https://drive.google.com/uc?export=download&id=1TwnmclaTGC348v6iqA8_OcUYINPI1DpA"
+response8 = requests.get(url8)
 logo = Image.open(BytesIO(response1.content))
+syafa = Image.open(BytesIO(response5.content))
+debby = Image.open(BytesIO(response6.content))
 kent = Image.open(BytesIO(response7.content))
+zahra = Image.open(BytesIO(response8.content))
 
 # Background and Theme Adjustment
 st.markdown(
@@ -45,14 +54,20 @@ st.write("""
 st.write("#### Meet The Team: Kelompok 1 Desain Proyek Teknik Biomedik 2021")
 team1, team2 = st.columns([1,3])
 with team1:
-    st.image(kent, width=200)
+    st.image(kent, width=150)
+    st.image(kent, width=150)
+    st.image(kent, width=150)
+    st.image(zahra, width=150)
 with team2:
     st.write("""
              #### Syafamillah Tsabitah
-             ###### 2106631816
+             2106631816
+
              #### Debby Rofiko Malik
-             ###### 2106705000
+             2106705000
+
              #### Kent Frederick Wirawan
-             ###### 2106706760
+             2106706760
+
              #### Fatimah Azzahra
-             ###### 2106732235""")
+             2106732235""")

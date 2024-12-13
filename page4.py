c09a625ea6e1e846a1750d28c12df98e786e051f
Kent Frederick Wirawan <kent.f.wirawan@gmail.com>
2024-12-13 15:54:30 +0700

    Add files via upload

--- page4.py
+++ page4.py
@@ -46,7 +46,9 @@ team1, team2 = st.columns([1,3])
 with team1:
     st.image(kent, width=200)
 with team2:
-    st.write("Kent Frederick Wirawan(2106706760)")
+    st.write("""
+             ## Kent Frederick Wirawan
+             ## (2106706760)""")
 # st.write(""" #### Meet The Team: Kelompok 1 Desain Proyek Teknik Biomedik 2021
 # Debby Rofiko Malik (2106705000)
 # Syafamillah Tsabitah (2106631816)  

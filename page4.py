3c68d6e5a2961eabc7a900e88cffa5960382e3a9
Kent Frederick Wirawan <kent.f.wirawan@gmail.com>
2024-12-16 12:04:11 +0700

    Update page4.py

--- page4.py
+++ page4.py
@@ -40,7 +40,7 @@ tit1, tit2 = st.columns([1,4])
 with tit1:
     st.image(logo, width=300)
 with tit2:
-    st.write("## pHocus Smart Patch")
+    st.write("## PHocus Smart Patch")
 st.write("""
 ##### A pH detector to measure your skin pH from just a photo of patch!
 """)

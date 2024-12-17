0a5b32379ac3a15b390c0da07e683fcbd3c61333
Kent Frederick Wirawan <kent.f.wirawan@gmail.com>
2024-12-17 11:02:09 +0700

    Update page3.py

--- page3.py
+++ page3.py
@@ -12,6 +12,7 @@ import requests
 from io import BytesIO
 
 logo = Image.open(BytesIO(requests.get("https://drive.google.com/uc?export=download&id=1EuzviqgyPz8Ve-jUqounzMGFLfBKKAQz").content))
+expectation = Image.open(BytesIO(requests.get("https://drive.google.com/uc?export=download&id=1diCJUAVNQcu2aRDJZA-afDHRELuhNR_E").content))
 
 # Background and Theme Adjustment
 st.markdown(
@@ -88,3 +89,27 @@ elif d<=0:
 elif d>=0:
     dcolor = "inverse"
 st.metric("### Prediction Results", value=res, delta=d, delta_color=dcolor)
+
+# Expected Outcome
+st.write("""
+### What Do We Expect?
+After the reaction, a new color would be appear of the mixture. But what does the color mean?
+""")
+st.image(expectation)
+exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8 = st.columns(8)
+with exp1:
+    st.write("  ")
+with exp2:
+    st.write("Control color")
+with exp3:
+    st.write("Your skin is dry")
+with exp4:
+    st.write("Your skin is normal")
+with exp5:
+    st.write("Your skin is still bleeding")
+with exp6:
+    st.write("Your skin has a mild bleeding")
+with exp7:
+    st.write("Your skin has a moderate bleeding")
+with exp8:
+    st.write("Your skin has a severe bleeding")
